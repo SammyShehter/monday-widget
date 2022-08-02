b927462a7577b734c8429ce664abd15e2abd4164
SammyShehter <sammyshehter@gmail.com>
2022-08-02 13:30:41 +0300

    design changes 3

--- islands/Card.tsx
+++ islands/Card.tsx
@@ -16,7 +16,7 @@ export default function Card(props: CardProps) {
     
     return (
             <div
-                class={tw`flex justify-center items-center flex-col rounded-md p-4 overflow-hidden ${viewed ? 'hidden': ''}`}
+                class={tw`flex justify-center items-center flex-col rounded-md p-4 overflow-hidden bg-white ${viewed ? 'hidden': ''}`}
             >
               <Tip/>
                 <button
